11d72b9b6449a9f8d1fd69e43cd98ae1488f6987
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-20 13:41:44 +0530

    reformat black

--- examples/train_step_example.py
+++ examples/train_step_example.py
@@ -12,58 +12,65 @@ from pytorch_cnn_trainer import engine
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	MODEL_NAME = "resnet18"
-	NUM_ClASSES = 10
-	IN_CHANNELS = 3
-	PRETRAINED = True  # If True -> Fine Tuning else Scratch Training
-	EPOCHS = 5
-	EARLY_STOPPING = True  # If you need early stoppoing for validation loss
-	SAVE_PATH = "{}.pt".format(MODEL_NAME)
-	SEED = 42
+    MODEL_NAME = "resnet18"
+    NUM_ClASSES = 10
+    IN_CHANNELS = 3
+    PRETRAINED = True  # If True -> Fine Tuning else Scratch Training
+    EPOCHS = 5
+    EARLY_STOPPING = True  # If you need early stoppoing for validation loss
+    SAVE_PATH = "{}.pt".format(MODEL_NAME)
+    SEED = 42
 
-	# Train and validation Transforms which you would like
-	train_transforms = T.Compose([T.ToTensor(), T.Normalize((0.5,), (0.5,))])
-	valid_transforms = T.Compose([T.ToTensor(), T.Normalize((0.5,), (0.5,))])
+    # Train and validation Transforms which you would like
+    train_transforms = T.Compose([T.ToTensor(), T.Normalize((0.5,), (0.5,))])
+    valid_transforms = T.Compose([T.ToTensor(), T.Normalize((0.5,), (0.5,))])
 
-	utils.seed_everything(SEED)
-	print("Setting Seed for the run, seed = {}".format(SEED))
+    utils.seed_everything(SEED)
+    print("Setting Seed for the run, seed = {}".format(SEED))
 
-	print("Creating Train and Validation Dataset")
-	train_set, valid_set = dataset.create_cifar10_dataset(train_transforms, valid_transforms)
-	print("Train and Validation Datasets Created")
+    print("Creating Train and Validation Dataset")
+    train_set, valid_set = dataset.create_cifar10_dataset(
+        train_transforms, valid_transforms
+    )
+    print("Train and Validation Datasets Created")
 
-	print("Creating DataLoaders")
-	train_loader, valid_loader = dataset.create_loaders(train_set, train_set)
-	print("Train and Validation Dataloaders Created")
+    print("Creating DataLoaders")
+    train_loader, valid_loader = dataset.create_loaders(train_set, train_set)
+    print("Train and Validation Dataloaders Created")
 
-	print("Creating Model")
-	model = model_factory.create_torchvision_model(MODEL_NAME, num_classes=NUM_ClASSES, pretrained=True)
-	if torch.cuda.is_available():
-	    print("Model Created. Moving it to CUDA")
-	else:
-	    print("Model Created. Training on CPU only")
-	model.to(device)
-	optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    print("Creating Model")
+    model = model_factory.create_torchvision_model(
+        MODEL_NAME, num_classes=NUM_ClASSES, pretrained=True
+    )
+    if torch.cuda.is_available():
+        print("Model Created. Moving it to CUDA")
+    else:
+        print("Model Created. Training on CPU only")
+    model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-	criterion = nn.CrossEntropyLoss()  # All classification problems we need Cross entropy loss
-	early_stopper = utils.EarlyStopping(patience=7, verbose=True, path=SAVE_PATH)
+    criterion = (
+        nn.CrossEntropyLoss()
+    )  # All classification problems we need Cross entropy loss
+    early_stopper = utils.EarlyStopping(patience=7, verbose=True, path=SAVE_PATH)
 
-	for epoch in tqdm(range(EPOCHS)):
-	    print()
-	    print("Training Epoch = {}".format(epoch))
-	    train_metrics = engine.train_step(model, train_loader, criterion, device, optimizer)
-	    print()
-	    print("Validating Epoch = {}".format(epoch))
-	    valid_metrics = engine.val_step(model, valid_loader, criterion, device)
-	    validation_loss = valid_metrics["loss"]
-	    early_stopper(validation_loss, model=model)
+    for epoch in tqdm(range(EPOCHS)):
+        print()
+        print("Training Epoch = {}".format(epoch))
+        train_metrics = engine.train_step(
+            model, train_loader, criterion, device, optimizer
+        )
+        print()
+        print("Validating Epoch = {}".format(epoch))
+        valid_metrics = engine.val_step(model, valid_loader, criterion, device)
+        validation_loss = valid_metrics["loss"]
+        early_stopper(validation_loss, model=model)
 
-	    if early_stopper.early_stop:
-	        print("Saving Model and Early Stopping")
-	        print("Early Stopping. Ran out of Patience for validation loss")
-	        break
+        if early_stopper.early_stop:
+            print("Saving Model and Early Stopping")
+            print("Early Stopping. Ran out of Patience for validation loss")
+            break
 
-	    print("Done Training, Model Saved to Disk")
-	    
+        print("Done Training, Model Saved to Disk")
