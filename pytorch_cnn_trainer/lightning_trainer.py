08e1d61b2e6b00a36de410f5592e79938762fbc9
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-30 12:02:19 +0530

    removes lightning for now

--- pytorch_cnn_trainer/lightning_trainer.py
+++ pytorch_cnn_trainer/lightning_trainer.py
@@ -1,5 +1,5 @@
-# Do the same thing with Lightning
-# We can use the config, model and dataset
-# We need to redefine the engine and train
+# # Do the same thing with Lightning
+# # We can use the config, model and dataset
+# # We need to redefine the engine and train
 
-import pytorch_lightning as pl
+# import pytorch_lightning as pl
