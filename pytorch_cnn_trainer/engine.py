a5af7c6c956bf027ccf6289eff0df8fbc840e547
Aditya Oke <47158509+oke-aditya@users.noreply.github.com>
2020-07-30 20:13:13 +0530

    Reformatted

--- pytorch_cnn_trainer/engine.py
+++ pytorch_cnn_trainer/engine.py
@@ -93,17 +93,19 @@ def train_step(
 
             if grad_penalty is True:
                 # Scales the loss for autograd.grad's backward pass, resulting in scaled grad_params
-                scaled_grad_params = torch.autograd.grad(fp16_scaler.scale(loss), model.parameters(), create_graph=True)
+                scaled_grad_params = torch.autograd.grad(
+                    fp16_scaler.scale(loss), model.parameters(), create_graph=True
+                )
                 # Creates unscaled grad_params before computing the penalty. scaled_grad_params are
                 # not owned by any optimizer, so ordinary division is used instead of fp16_scaler.unscale_:
-                inv_scale = 1. / fp16_scaler.get_scale()
+                inv_scale = 1.0 / fp16_scaler.get_scale()
                 grad_params = [p * inv_scale for p in scaled_grad_params]
                 # Computes the penalty term and adds it to the loss
                 with amp.autocast():
                     grad_norm = 0
                     for grad in grad_params:
                         grad_norm += grad.pow(2).sum()
-                    
+
                     grad_norm = grad_norm.sqrt()
                     loss = loss + grad_norm
 
@@ -112,7 +114,6 @@ def train_step(
             fp16_scaler.step(optimizer)
             # Update for next iteration
             fp16_scaler.update()
-
 
         else:
             output = model(inputs)
